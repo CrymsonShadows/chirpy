abfe358900377ef83b5e73a265d41961041dd7be
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-02-12 18:52:26 -0600

    refactor and log message server is running on a port

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	filepathRoot := "/"
+	port := "8080"
 	mux.Handle("/", http.FileServer(http.Dir(".")))
-	server := http.Server{Handler: mux, Addr: ":8080"}
-	err := server.ListenAndServe()
-	fmt.Println(err)
+	server := &http.Server{Handler: mux, Addr: ":" + port}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
