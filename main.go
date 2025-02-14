11304dafd3a7cd21a270d5ef9accff69a80687a2
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-02-13 21:44:19 -0600

    change fileserver path to user /app/ path and add readiness endpoint /healthz

--- main.go
+++ main.go
@@ -9,7 +9,16 @@ func main() {
 	mux := http.NewServeMux()
 	filepathRoot := "/"
 	port := "8080"
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Println("Error writing response:", err)
+			return
+		}
+	})
 	server := &http.Server{Handler: mux, Addr: ":" + port}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
