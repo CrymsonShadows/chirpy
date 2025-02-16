71381184d1b7f59f89b6b4d4c8f73c92c5adb746
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-02-15 18:31:39 -0600

    change the metrics endpoint to be under admin and returns html

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerRequestCount)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerRequestCount)
 	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
@@ -51,9 +51,16 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 }
 
 func (cfg *apiConfig) handlerRequestCount(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	_, err := w.Write([]byte(fmt.Sprintf(
+		`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`,
+		cfg.fileserverHits.Load())))
 	if err != nil {
 		log.Println("Error writing response:", err)
 		return
