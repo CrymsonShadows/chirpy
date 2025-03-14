d15a8626ec1ca78b75ac6bf8d1993577d08598d9
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2025-03-14 17:46:27 -0500

    Add response message to reset

--- reset.go
+++ reset.go
@@ -10,12 +10,14 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
 		Error string `json:"error,omitempty"`
 	}
 
-	cfg.fileserverHits.Store(0)
 	if cfg.platform != "dev" {
 		log.Printf("Platform not in dev")
 		w.WriteHeader(403)
+		w.Write([]byte("Reset is only allowed in dev environment."))
 		return
 	}
+
+	cfg.fileserverHits.Store(0)
 
 	w.WriteHeader(http.StatusOK)
 	err := cfg.db.ResetUsers(req.Context())
@@ -24,4 +26,5 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
 		respondWithJSON(w, 500, responseVals{Error: "Something went wrong"})
 		return
 	}
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
 }
